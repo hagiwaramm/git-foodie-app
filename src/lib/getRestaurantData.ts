dec267e81d906ceb8d132b4dd3a3b58f740d302e
hagiwaram <>
2026-02-13 16:45:10 +0900

    デバッグ2

--- src/lib/getRestaurantData.ts
+++ src/lib/getRestaurantData.ts
@@ -4,8 +4,7 @@ export async function getRestaurantData(param: string) {
 
   // 修正：ドメインの後に正しいパスを追加し、https:// を付ける
   const url = `/hotpepper/gourmet/v1/?key=${APIKEY}&format=json&${param}`;
-
-
+  console.log(url);
   try {
     const response = await fetch(url);
     if (!response.ok) {
